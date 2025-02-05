12db80d4e8493838c16efd48b64afe7dc9006882
3ly-Al-husseiny <ali.ahmed.software.engineer@gmail.com>
2025-02-05 18:02:05 +0200

    Add files via upload

---
+++ Day 3/Demo/js/script.js
@@ -0,0 +1,99 @@
+// // output
+// document.writeln("<h1>3lawy learns java script</h1>")
+// document.writeln("Hello World")
+
+// // console logs 
+// console.log("console.log --> Hello world")
+// console.error("console.error ---> error messages")
+// console.error("console.error ---> error messages")
+
+
+// // variables 
+// // 1. var
+// // 2. let
+// // 3. const
+// // 4. Automatically 
+
+// // The naming case in javascipt is the amelCase
+// var ali = "Ali";
+// var ahmed = "Ahmed";
+// var aliAhmed = ali+" "+ahmed;
+// console.log(aliAhmed)
+// aliAhmed = 20;
+// console.log(aliAhmed)
+
+
+// document.getElementsByClassName("title")[0].innerHTML = aliAhmed
+
+
+// // Declaration 
+// var age
+
+// // Assign
+// age = 21 
+
+
+// // Defination = declaration + assigning
+// var age2 = 21
+
+// console.log(age2)
+
+
+// let name = "Ali";
+// console.writeln(name);
+
+
+// const text = ali; // Can't Be changed in the Future. 
+
+
+
+
+// // var 
+// var VAR;
+// VAR = "VAR";
+// VAR = "VAR2"
+// var VAR = "VAR3"
+
+
+// // let
+// let LET;
+// LET = "LET";
+// LET = "LET2";
+// // let LET = "LET3" // Invalid --> You can't re-declare the let variables in the same scope 
+
+
+// // Const
+// //const CONST; // Invalid --> You should initialzie the const variables at the same time of the declaration --> Definition
+// //const CONST2 = "CONST";
+// CONST2 = "CONST2"; // 
+// const CONST2 = "CONST3";
+
+// // Any Errors in JavaScript ----> 
+
+// // Errors in JavaScript code will appear in the Console 
+
+
+
+// // math object 
+
+
+// var fname = " Ali ,Ahmed, Alhusseiny "
+// console.log(fname.slice(0,5));
+// console.log(fname.substring(0,5));
+// console.log(fname.split(","));
+// console.log(fname.trim());
+// console.log(fname.length); //
+// console.log(fname.toUpperCase());
+// console.log(fname.toLowerCase());
+// console.log(fname.concat(" Awad "));
+// console.log(fname.replace(" Ali "," Reda ")) //
+// console.log(fname.repeat(10))
+
+
+
+
+
+
+
+
+
